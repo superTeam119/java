6d4b4e3a0f194904fad8477da0094e059a10d199
user <user@samsung>
2018-02-20 20:31:04 +0200

    new interface

---
+++ DictionaryApplication/src/FxmlFiles/FXMLDictionaryController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FxmlFiles;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author user
+ */
+public class FXMLDictionaryController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }   
+    
+   public void FriendsAndEnemies(){}
+   public void Methods(){} 
+   public void Back(){}
+   public void CreateDictionary(){}
+}
